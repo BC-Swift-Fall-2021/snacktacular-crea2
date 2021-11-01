b686abceb4b838b9f1aa061f03adcc4c2595e7c9
crea2 <reacc@bc.edu>
2021-11-01 13:35:03 -0400

    Finished 8.7

---
+++ Snacktacular1/APIkeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIkeys.swift
+//  Snacktacular1
+//
+//  Created by Christopher Rea on 11/1/21.
+//
+
+import Foundation
+
+struct APIKey {
+    static let googlePlacesKey = "AIzaSyDpmBXxFeL8GbO1Y1P0q4uPvpPMSYRP-Ko"
+}
